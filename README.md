c8e8669f81b34b2d39d64e27590876001b7912d9
Karabo <karabomatabane12@gmail.com>
2022-08-31 05:33:42 +0200

    The Main framework

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # This is where I get to learn and apply code
 ## All the best to me
+### Is this what I might end up doing for life?
---
+++ main.py
@@ -0,0 +1,2 @@
+# This is where I will be doing code related to the main project
+# I will create branches within this which I will be doing my exercises and practicing my coding skills
