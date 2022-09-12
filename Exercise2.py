0468b66b6aae313e4f7ef7fbf64a86ace88d2591
Karabo <karabomatabane12@gmail.com>
2022-09-12 19:34:06 +0200

    The Main framework

---
+++ Exercise2.py
@@ -0,0 +1,25 @@
+# Given a Python list you should be able to display Python list in descending order
+aList = [100, 200, 300, 400, 500]
+
+# Concatenate two lists index-wise
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+
+# Given a Python list. Turn every item of a list into its square
+bList = [1, 2, 3, 4, 5, 6, 7]
+
+# Add item 7000 after 6000 in the following Python List
+list3 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+# Given a Python list, find value 20 in the list, and if it is present, replace it with 200.
+# Only update the first occurrence of a value
+list4 = [5, 10, 15, 20, 25, 50, 20]
+
+# Access value from the following tuple, say 30
+aTuple = ("Orange", [10, 20, 30], (5, 15, 25))
+
+# Create a tuple with single item
+
+# note: atuple = (10) is not a tuple
+num = (5)
+type(num) # is an int type
