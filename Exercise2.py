624c5746e9d1b6a2dea3d138cb71744460e070b5
Karabo <karabomatabane12@gmail.com>
2022-09-12 19:46:58 +0200

    The Main framework

--- Exercise2.py
+++ Exercise2.py
@@ -23,3 +23,71 @@ aTuple = ("Orange", [10, 20, 30], (5, 15, 25))
 # note: atuple = (10) is not a tuple
 num = (5)
 type(num) # is an int type
+
+# Unpack the following tuple into 4 variables
+cTuple = (10, 20, 30, 40)
+
+# Swap the following two tuples
+tuple1 = (11, 22)
+tuple2 = (99, 88)
+
+# Counts the number of occurrences of item 50 from a tuple
+tuple3 = (50, 10, 60, 70, 50)
+
+# Two lists converted into a dictionary
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+# Merging two dictionaries into one
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+# Rename key item in a dictionary
+sampleDict = {
+    "name": "Kelly",
+    "age":25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Get the key corresponding to the minimum value
+sampleDict_1 = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+
+# Create a new dictionary by extracting: 'name' and 'salary'
+sampleDict_2 = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york" }
+
+# Add a list of elements to a given set
+sampleSet = {"Yellow", "Orange", "Black"}
+sampleList = ["Blue", "Green", "Red"]
+
+# Return a set of identical items from a given two Python set
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+# Returns a new set with all items from both sets by removing duplicates
+set_1 = {10, 20, 30, 40, 50}
+set_2 = {30, 40, 50, 60, 70}
+
+# Given two Python sets, update first set with items that exist only in the first set and not in the
+# second set.
+set3 = {10, 20, 30}
+set4 = {20, 40, 50}
+
+# Remove 10, 20, 30 elements from a following set at once
+set_3 = {10, 20, 30, 40, 50}
+
+# Return a set of all elements in either A or B, but not both
+set_4 = {10, 20, 30, 40, 50}
+set5 = {30, 40, 50, 60, 70}
+
+# Remove items from set1 that are not common to both set1 and set2
+set_5 = {10, 20, 30, 40, 50}
+set6 = {30, 40, 50, 60, 70}
+
